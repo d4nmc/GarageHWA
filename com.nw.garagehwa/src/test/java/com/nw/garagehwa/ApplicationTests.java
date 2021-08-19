c4fdc6fea50b56d429b3b675a1bdf77b82b277e4
d4nmc <danielmcgee.work@gmail.com>
2021-08-19 16:31:15 +0100

    MC: started again, again

---
+++ com.nw.garagehwa/src/test/java/com/nw/garagehwa/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nw.garagehwa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
